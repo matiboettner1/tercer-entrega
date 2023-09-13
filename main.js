08ebb58829faa5279c022b58b0ede16a4f7a0fd8
“matiboettner1” <matiasboettner2004@gmail.com>
2023-09-13 14:50:08 -0300

    crea function para aplicar un descuento del 20% al valor total

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const RESPUESTA_POSITIVA = 'si';
+const RESPUESTA_NEGATIVA = 'no';
+
 function preguntarUsuarioSiCompra() {
-    const RESPUESTA_POSITIVA = 'si';
-    const RESPUESTA_NEGATIVA = 'no';
     let usuarioQuiereComprar = prompt('Bienvenido a hechoencasa.cb. ¿Desea comprar en nuestra página? (Ingrese si o no)').toLowerCase().trim();
 
     while (usuarioQuiereComprar !== RESPUESTA_POSITIVA && usuarioQuiereComprar !== RESPUESTA_NEGATIVA) {
@@ -18,7 +19,7 @@ function preguntarUsuarioSiCompra() {
 function agregarProductos() {
     const LIMITE_PRODUCTOS = 10;
     const CRITERIO_CORTE = 'listo';
-    let budin;
+    let compraBudines;
     let tiposDeBudines = {
         'budin de zanahoria': 1500,
         'budin de frutos rojos': 2000,
@@ -28,23 +29,47 @@ function agregarProductos() {
         'budin de frutos secos': 1800
     };
 
-    console.log(tiposDeBudines);
+    console.table(tiposDeBudines);
 
     let total = 0;
 
     for (let i = 1; i <= LIMITE_PRODUCTOS; i++) {
-        budin = prompt('Ingrese el o los budines que desea comprar uno por uno (con el formato de "Budin de -tipo de budin-"). Los tipos se pueden ver en la consola. Cuando haya terminado, ingrese la palabra "Listo".').toLowerCase();
+        compraBudines = prompt('Ingrese el o los budines que desea comprar uno por uno (con el formato de "Budin de -tipo de budin-"). Los tipos se pueden ver en la consola. Cuando haya terminado, ingrese la palabra "Listo".').toLowerCase();
 
-        if (budin.toLowerCase().trim() == CRITERIO_CORTE) {
+        if (compraBudines.toLowerCase().trim() == CRITERIO_CORTE) {
             break;
         }
 
-        if (tiposDeBudines[budin]) {
-            total += tiposDeBudines[budin];
+        if (tiposDeBudines[compraBudines]) {
+            total += tiposDeBudines[compraBudines];
         }
     }
 
     alert('Usted tendría que pagar un total de $' + total);
+
+    let usuarioQuiereUsarCupon = prompt('Desea aplicar cupon de 20% de descuento? (Ingrese si o no)')
+
+
+    while (usuarioQuiereUsarCupon !== RESPUESTA_POSITIVA && usuarioQuiereUsarCupon !== RESPUESTA_NEGATIVA) {
+        alert('No entiendo tu respuesta. Por favor, ingrese "si" o "no".');
+        usuarioQuiereUsarCupon = prompt('Desea aplicar cupon de 20% de descuento? (Ingrese si o no)').toLowerCase().trim();
+    }
+
+    if (usuarioQuiereUsarCupon.trim().toLowerCase() === RESPUESTA_POSITIVA) {
+        let porcentajeDescuento = 20;
+        aplicarDescuento(total, porcentajeDescuento);
+    } else{
+        alert('Usted tendría que pagar un total de $' + total);
+    }
+
 }
+
+function aplicarDescuento (valorOriginal, porcentajeDescuento) {
+    let descuento = (valorOriginal * porcentajeDescuento) / 100;
+    let valorConDescuento = valorOriginal - descuento;
+    return valorConDescuento;
+}
+
+
 
 preguntarUsuarioSiCompra();
